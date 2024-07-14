b4d9a13510df1f666312fd01314b0fa59bf388ee
„justthatrandomcoder <juliusgrimm46@gmail.com>
2024-07-14 18:45:19 +0200

    added text to the predection

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def get_weather_forecast():
 if __name__ == '__main__':
     from waitress import serve
     #serve(app, host="0.0.0.0", port=8008)
-    app.run(host="0.0.0.0", port=8029)
+    app.run(host="0.0.0.0", port=8030)
